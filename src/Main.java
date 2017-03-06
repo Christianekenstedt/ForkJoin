e06294c094623bcd390f4037f91fc8d8365e49b6
christianekenstedt <christian>
2017-03-06 16:04:51 +0100

    Update to float

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.math.*;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        float[] mergeArray = new float[50];
+        float[] quickArray = new float[50];
+
+
+        for (int i = 0; i < 50; i++){
+            mergeArray[i] = (float)Math.random();
+            quickArray[i] = (float)Math.random();
+        }
+
+        System.out.println("mergeArray " + Arrays.toString(mergeArray));
+        System.out.println("quickArray " + Arrays.toString(quickArray));
+
+        MergeSort.sort(mergeArray);
+        QuickSort.sort(quickArray);
+
+        System.out.println(checkIfSorted(mergeArray) + " - mergeArray " + Arrays.toString(mergeArray));
+        System.out.println(checkIfSorted(quickArray) + " - quickArray " + Arrays.toString(quickArray));
+
+
+
+
+    }
+
+    public static boolean checkIfSorted(float[] arr){
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
